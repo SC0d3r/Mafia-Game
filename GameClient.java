5432aa20658e2c65d716176339d27f6ef26ae1eb
SC0d3r <sasan.kelishani@gmail.com>
2021-05-20 23:36:30 +0430

    add functionality to disable/enable chat

--- GameClient.java
+++ GameClient.java
@@ -1,15 +1,38 @@
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
 public class GameClient {
   private String hostname;
   private int port;
   private String username;
+  // private Set<String> inGameUsernames;
+  private boolean isChatDisabled;
 
   public GameClient(String hostname, int port) {
     this.port = port;
     this.hostname = hostname;
+    // this.inGameUsernames = GameClient.fetchUsernames("usernames.txt");
+    this.isChatDisabled = false;
+  }
+
+  public void setIsChatDisabled(boolean status) {
+    this.isChatDisabled = status;
+  }
+
+  public boolean getIsChatDisabled() {
+    return this.isChatDisabled;
+  }
+
+  public boolean doesUsernameExists(String username) {
+    // TODO: add check for duplicate usernames
+    return false;
+    // return this.inGameUsernames.contains(username);
   }
 
   public void execute() {
@@ -18,6 +41,11 @@ public class GameClient {
       System.out.println("Successfully connected to server!");
 
       new ReadThread(socket, this).start();
+      try {
+        Thread.sleep(10);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
       new WriteThread(socket, this).start();
 
     } catch (UnknownHostException ex) {
@@ -45,5 +73,17 @@ public class GameClient {
 
     GameClient gameClient = new GameClient(hostname, port);
     gameClient.execute();
+  }
+
+  public static Set<String> fetchUsernames(String fileName) {
+    try (Scanner in = new Scanner(new FileReader(fileName))) {
+      String usernames = in.nextLine().trim();
+      System.out.println("all usernames : " + usernames);
+      return new HashSet<String>(Arrays.asList(usernames.split(",")));
+    } catch (Exception ex) {
+      System.out.println("Error while reading usernames : " + ex.getMessage());
+      ex.printStackTrace();
+      return new HashSet<>();
+    }
   }
 }
