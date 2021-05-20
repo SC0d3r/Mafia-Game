a5b11b6b9fd51b0e7c458560f050f89de4215a4b
SC0d3r <sasan.kelishani@gmail.com>
2021-05-20 23:35:06 +0430

    add SocketDataReciever to output and parse the messages

--- ReadThread.java
+++ ReadThread.java
@@ -10,10 +10,12 @@ public class ReadThread extends Thread {
   private BufferedReader reader;
   private Socket socket;
   private GameClient client;
+  private SocketDataReciever socketData;
 
   public ReadThread(Socket socket, GameClient client) {
     this.socket = socket;
     this.client = client;
+    this.socketData = new SocketDataReciever();
 
     try {
       InputStream input = socket.getInputStream();
@@ -29,11 +31,51 @@ public class ReadThread extends Thread {
     while (true) {
       try {
         String response = reader.readLine();
-        System.out.println("\n" + response);
+        // GameServer.clrscr();
 
-        if (this.client.getUsername() != null) {
-          System.out.print("<" + this.client.getUsername() + ">: ");
+        if (this.socketData.isGameBeginCommand(response)) {
+          this.socketData.clearChatMessages();
+          this.socketData.clearHeader();
         }
+        if (this.socketData.isAddAndClearChatCommand(response)) {
+          this.socketData.saveAndClearChatMessages();
+        }
+
+        this.socketData.addInfo(response);
+        this.socketData.removeHeaderInfo(response);
+
+        if (!this.client.getIsChatDisabled()) {
+          this.socketData.addChatMessage(response);
+          this.socketData.addChatCommand(response);
+        }
+
+        System.out.println(this.socketData.getHeaderBarInformations());
+        System.out.println(this.socketData.getChatMessages());
+
+        if (this.socketData.shouldDisableChat(response))
+          this.client.setIsChatDisabled(true);
+        if (this.socketData.shouldEnableChat(response))
+          this.client.setIsChatDisabled(false);
+
+        if (!this.socketData.isResponseCommand(response))
+          System.out.println(response);
+
+        try {
+          Thread.sleep(10);// to add the time needed to change the user name otherwise username is still
+                           // null!
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+
+        if (this.client.getIsChatDisabled()) {
+          System.out.print("<You are Sleep>");
+          continue;
+        }
+        if (this.client.getUsername() != null) {
+          System.out.println("\n-------------------------------------------------------------");
+          System.out.print(this.client.getUsername() + ": ");
+        } else
+          System.out.print("Enter username: ");
       } catch (IOException ex) {
         System.out.println("Error on reading from server: " + ex.getMessage());
         ex.printStackTrace();
@@ -41,4 +83,5 @@ public class ReadThread extends Thread {
       }
     }
   }
+
 }
