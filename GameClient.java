3a3f86e05bf220eb710946daf1df0f504d599c88
SC0d3r <sasan.kelishani@gmail.com>
2021-05-23 17:22:04 +0430

    Remove comments

--- GameClient.java
+++ GameClient.java
@@ -1,26 +1,17 @@
-import java.io.FileReader;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
 
 public class GameClient {
   private String hostname;
   private int port;
   private String username;
-  // private Set<String> inGameUsernames;
-  // private boolean isChatDisabled;
   private Player player;
   private boolean isInMayorVotingState;
 
   public GameClient(String hostname, int port) {
     this.port = port;
     this.hostname = hostname;
-    // this.inGameUsernames = GameClient.fetchUsernames("usernames.txt");
-    // this.isChatDisabled = false;
     this.isInMayorVotingState = false;
   }
 
@@ -40,15 +31,10 @@ public class GameClient {
     return this.player.getRole() == role;
   }
 
-  // public void setIsChatDisabled(boolean status) {
-  // this.isChatDisabled = status;
-  // }
-
   public boolean getCanChat() {
     if (this.player == null)
       return true;
     return this.player.getCanChat();
-    // return this.isChatDisabled;
   }
 
   public boolean isAlive() {
@@ -60,7 +46,6 @@ public class GameClient {
   public boolean doesUsernameExists(String username) {
     // TODO: add check for duplicate usernames
     return false;
-    // return this.inGameUsernames.contains(username);
   }
 
   public void execute() {
@@ -70,11 +55,7 @@ public class GameClient {
 
       SocketDataReciever socketData = new SocketDataReciever();
       new ReadThread(socket, this, socketData).start();
-      try {
-        Thread.sleep(10);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
+      UTIL.sleep(10);
       new WriteThread(socket, this, socketData).start();
 
     } catch (UnknownHostException ex) {
@@ -104,15 +85,4 @@ public class GameClient {
     gameClient.execute();
   }
 
-  public static Set<String> fetchUsernames(String fileName) {
-    try (Scanner in = new Scanner(new FileReader(fileName))) {
-      String usernames = in.nextLine().trim();
-      System.out.println("all usernames : " + usernames);
-      return new HashSet<String>(Arrays.asList(usernames.split(",")));
-    } catch (Exception ex) {
-      System.out.println("Error while reading usernames : " + ex.getMessage());
-      ex.printStackTrace();
-      return new HashSet<>();
-    }
-  }
 }
