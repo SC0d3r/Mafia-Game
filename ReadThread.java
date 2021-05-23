0fdcdd795949ed1ca9cd68f84f0ef4715befe698
SC0d3r <sasan.kelishani@gmail.com>
2021-05-23 17:22:47 +0430

    refactor run method

--- ReadThread.java
+++ ReadThread.java
@@ -32,119 +32,115 @@ public class ReadThread extends Thread {
         String response = reader.readLine();
         GameServer.clrscr();
 
-        // if (response.equals(SocketDataSender.CLEAR_NEWS)) {
+        this.executeCommand(response);
 
-        // System.out.println("HERE CLEAE NEWS");
-        // try {
-        // Thread.sleep(3000);
-        // } catch (InterruptedException e) {
-        // // TODO Auto-generated catch block
-        // e.printStackTrace();
-        // }
-        // this.socketData.clearNews();
-        // }
+        this.addAndUpdateData(response);
 
-        if (response.equals(SocketDataSender.DISABLE_VOTING)) {
-          this.socketData.disableVoting();
-          this.socketData.setIsVotingInProgress(false);
-        }
+        this.draw(response);
 
-        if (this.socketData.shouldEnableVoting()) {
-          this.socketData.setIsVotingInProgress(true);
-        }
+        this.printPromptText();
 
-        if (this.socketData.isGameBeginCommand(response)) {
-          this.socketData.clearChatMessages();
-          this.socketData.clearHeader();
-        }
-
-        if (this.socketData.isSendPlayerStateCommand(response)) {
-          this.client.setPlayer(this.socketData.extractPlayerState(response));
-        }
-
-        if (this.socketData.isAddAndClearChatCommand(response)) {
-          this.socketData.saveAndClearChatMessages();
-        }
-
-        if (this.socketData.isStartMayorVotingState(response)) {
-          this.client.setIsInMayorVotingState(true);
-        }
-
-        if (this.socketData.isEndMayorVotingState(response)) {
-          this.client.setIsInMayorVotingState(false);
-        }
-
-        this.socketData.addVotingTable(response);
-        this.socketData.updateVotingTable(response);
-        this.socketData.addInfo(response);
-        this.socketData.removeHeaderInfo(response);
-
-        this.socketData.addNews(response);
-
-        if (this.client.getCanChat()) {
-          this.socketData.addChatMessage(response);
-          this.socketData.addChatCommand(response);
-        }
-
-        System.out.println(this.socketData.getHeaderBarInformations());
-        System.out.println(this.socketData.getNews());
-        System.out.println(this.socketData.getChatMessages());
-
-        if (this.socketData.getIsVotingInProgress()) {
-          String votingTable = this.socketData.getVotingTable();
-          System.out.println(votingTable);
-        }
-
-        // if (this.socketData.shouldDisableChat(response))
-        // this.client.setIsChatDisabled(true);
-        // if (this.socketData.shouldEnableChat(response))
-        // this.client.setIsChatDisabled(false);
-
-        if (!this.socketData.isResponseCommand(response))
-          System.out.println(response);
-
-        try {
-          Thread.sleep(10);// to add the time needed to change the user name otherwise username is still
-                           // null!
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
-
-        if (!this.client.isAlive()) {
-          System.out.println("\n-------------------------------------------------------------");
-          System.out.print("[*] You are dead.");
-          continue;
-        }
-
-        if (this.socketData.getIsVotingInProgress()) {
-          System.out.println("\n-------------------------------------------------------------");
-          System.out.print("Press Enter to start voting ...");
-          continue;
-        }
-
-        if (this.client.getIsInMayorVotingState()) {
-          if (!this.client.isRole(ROLE.MAYOR))
-            System.out.print("Mayor is deciding ...");
-          else
-            System.out.print("Press Enter to start decisioning on voting ...");
-          continue;
-        }
-
-        if (!this.client.getCanChat() && this.client.isAlive()) {
-          System.out.print("<.. zzZZZzz ..>");
-          continue;
-        }
-
-        if (this.client.getUsername() != null) {
-          System.out.println("\n-------------------------------------------------------------");
-          System.out.print(this.client.getUsername() + ": ");
-        } else
-          System.out.print("Enter username: ");
       } catch (IOException ex) {
         System.out.println("Error on reading from server: " + ex.getMessage());
         ex.printStackTrace();
         break;
       }
+    }
+  }
+
+  private void printPromptText() {
+    if (!this.client.isAlive()) {
+      System.out.println("\n-------------------------------------------------------------");
+      System.out.print("[*] You are dead.");
+      return;
+    }
+
+    if (this.socketData.getIsVotingInProgress()) {
+      System.out.println("\n-------------------------------------------------------------");
+      System.out.print("Press Enter to start voting ...");
+      return;
+    }
+
+    if (this.client.getIsInMayorVotingState()) {
+      if (!this.client.isRole(ROLE.MAYOR))
+        System.out.print("Mayor is deciding ...");
+      else
+        System.out.print("Press Enter to start decisioning on voting ...");
+      return;
+    }
+
+    if (!this.client.getCanChat() && this.client.isAlive()) {
+      System.out.print("<.. zzZZZzz ..>");
+      return;
+    }
+
+    if (this.client.getUsername() != null) {
+      System.out.println("\n-------------------------------------------------------------");
+      System.out.print(this.client.getUsername() + ": ");
+    } else
+      System.out.print("Enter username: ");
+  }
+
+  private void draw(String response) {
+    System.out.println(this.socketData.getHeaderBarInformations());
+    System.out.println(this.socketData.getNews());
+    System.out.println(this.socketData.getChatMessages());
+
+    if (this.socketData.getIsVotingInProgress()) {
+      String votingTable = this.socketData.getVotingTable();
+      System.out.println(votingTable);
+    }
+
+    if (!this.socketData.isResponseCommand(response))
+      System.out.println(response);
+
+    UTIL.sleep(10);// to add the time needed to change the user name otherwise username is still
+                   // null!
+  }
+
+  private void addAndUpdateData(String response) {
+    this.socketData.addVotingTable(response);
+    this.socketData.updateVotingTable(response);
+    this.socketData.addInfo(response);
+    this.socketData.removeHeaderInfo(response);
+
+    this.socketData.addNews(response);
+
+    if (this.client.getCanChat()) {
+      this.socketData.addChatMessage(response);
+      this.socketData.addChatCommand(response);
+    }
+  }
+
+  private void executeCommand(String response) {
+    if (response.equals(SocketDataSender.DISABLE_VOTING)) {
+      this.socketData.disableVoting();
+      this.socketData.setIsVotingInProgress(false);
+    }
+
+    if (this.socketData.shouldEnableVoting()) {
+      this.socketData.setIsVotingInProgress(true);
+    }
+
+    if (this.socketData.isGameBeginCommand(response)) {
+      this.socketData.clearChatMessages();
+      this.socketData.clearHeader();
+    }
+
+    if (this.socketData.isSendPlayerStateCommand(response)) {
+      this.client.setPlayer(this.socketData.extractPlayerState(response));
+    }
+
+    if (this.socketData.isAddAndClearChatCommand(response)) {
+      this.socketData.saveAndClearChatMessages();
+    }
+
+    if (this.socketData.isStartMayorVotingState(response)) {
+      this.client.setIsInMayorVotingState(true);
+    }
+
+    if (this.socketData.isEndMayorVotingState(response)) {
+      this.client.setIsInMayorVotingState(false);
     }
   }
 
--- WriteThread.java
+++ WriteThread.java
@@ -18,11 +18,7 @@ public class WriteThread extends Thread {
     this.dataSender = new SocketDataSender();
 
     try {
-      try {
-        Thread.sleep(10);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
+      UTIL.sleep(10);
       OutputStream output = socket.getOutputStream();
       this.wirter = new PrintWriter(output, true);
     } catch (IOException ex) {
@@ -39,7 +35,19 @@ public class WriteThread extends Thread {
   @Override
   public void run() {
     Console console = System.console();
+    String username = this.promptUsername(console);
+    this.createPromptLoop(console, username);
+    this.closeSocket();
+  }
 
+  private void createPromptLoop(Console console, String username) {
+    String text = "";
+    do {
+      text = this.promptUser(console, username);
+    } while (!text.equals("exit"));
+  }
+
+  private String promptUsername(Console console) {
     boolean isUserAdded = false;
     String username = "";
     while (!isUserAdded) {
@@ -54,71 +62,73 @@ public class WriteThread extends Thread {
       this.client.setUsername(username);
       isUserAdded = true;
     }
+    return username;
+  }
 
-    String text = "";
-    do {
-      if (!this.client.isAlive()) {
-        try {
-          Thread.sleep(100);
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
-        continue;
-      }
-      if (this.client.getIsInMayorVotingState()) {
-        if (this.client.isRole(ROLE.MAYOR)) {
-          while (true) {
-            if (!this.client.getIsInMayorVotingState() && !this.client.isAlive())
-              break;
-            text = console.readLine("<> Do you want to cancel voting [Y/N]? ").trim();
-            if (this.isAValidMayorVote(text))
-              break;
-          }
-
-          System.out.println("You voted: " + text);
-          if (this.client.getIsInMayorVotingState() && this.client.isAlive()) {// this is for if condition changes while
-                                                                               // stuck in above loop
-            // System.out.println("HERE");
-            this.wirter.println(this.dataSender.createMayorVote(text));
-          }
-        }
-        if (this.client.getIsInMayorVotingState())// this is for if condition changes while stuck in above loop
-          continue;
-      }
-
-      if (this.socketData.getIsVotingInProgress()) {
-        while (true) {
-          if (!this.socketData.getIsVotingInProgress() || !this.client.isAlive())
-            break;
-          text = console.readLine("Vote: ").trim();
-          if (this.socketData.isValidVote(text))
-            break;
-
-          if (this.socketData.getIsVotingInProgress())
-            System.out.println("<   Not a valid Vote  : " + text + " >");
-        }
-        if (this.socketData.getIsVotingInProgress() && this.client.isAlive()) {
-          this.wirter.println(this.dataSender.createVotingMapForServer(username, text, this.socketData.getVotes()));
-          continue;
-        }
-      }
-
-      if (this.client.getCanChat() && this.client.isAlive()) {
-        text = console.readLine(username + ": ").trim();
-        if (text.isBlank())
-          continue;
-
-        if (this.client.getCanChat() && this.client.isAlive())// maybe state changed while stuck in readline function
-          this.wirter.println(text);
-      }
-    } while (!text.equals("exit"));
-
+  private void closeSocket() {
     try {
       this.socket.close();
     } catch (IOException ex) {
       System.out.println("Error on writinerver: " + ex.getMessage());
 
     }
+  }
+
+  private String promptUser(Console console, String username) {
+    String text = "";
+
+    if (!this.client.isAlive()) {
+      text = console.readLine().trim().toLowerCase();// for player who wants to type 'exit' to quit the game
+      return text;
+    }
+
+    if (this.client.getIsInMayorVotingState()) {
+      if (this.client.isRole(ROLE.MAYOR)) {
+        while (true) {
+          if (!this.client.getIsInMayorVotingState() && !this.client.isAlive())
+            break;
+          text = console.readLine("<> Do you want to cancel voting [Y/N]? ").trim();
+          if (this.isAValidMayorVote(text))
+            break;
+        }
+
+        System.out.println("You voted: " + text);
+        if (this.client.getIsInMayorVotingState() && this.client.isAlive()) {// this is for if condition changes while
+                                                                             // stuck in above loop
+          // System.out.println("HERE");
+          this.wirter.println(this.dataSender.createMayorVote(text));
+        }
+      }
+      if (this.client.getIsInMayorVotingState())// this is for if condition changes while stuck in above loop
+        return text;
+    }
+
+    if (this.socketData.getIsVotingInProgress()) {
+      while (true) {
+        if (!this.socketData.getIsVotingInProgress() || !this.client.isAlive())
+          break;
+        text = console.readLine("Vote: ").trim();
+        if (this.socketData.isValidVote(text))
+          break;
+
+        if (this.socketData.getIsVotingInProgress())
+          System.out.println("<   Not a valid Vote  : " + text + " >");
+      }
+      if (this.socketData.getIsVotingInProgress() && this.client.isAlive()) {
+        this.wirter.println(this.dataSender.createVotingMapForServer(username, text, this.socketData.getVotes()));
+        return text;
+      }
+    }
+
+    if (this.client.getCanChat() && this.client.isAlive()) {
+      text = console.readLine(username + ": ").trim();
+      if (text.isBlank())
+        return text;
+      if (this.client.getCanChat() && this.client.isAlive())// maybe state changed while stuck in readline function
+        this.wirter.println(text);
+    }
+
+    return text;
   }
 
 }
