58a35a8d9ba9f1b15a62eec4f50897fc436a9d70
SC0d3r <sasan.kelishani@gmail.com>
2021-05-24 21:14:41 +0430

    remove sendNews from BeginNightState

--- BeginNightState.java
+++ BeginNightState.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
-import java.util.concurrent.TimeoutException;
 
-public class BeginNightState extends GameState {
+public class BeginNightState extends ServerState {
   private GameData gameData;
   private SocketDataSender dataSender;
 
@@ -16,20 +15,8 @@ public class BeginNightState extends GameState {
     this.gameData.updateDayTime(DAYTIME.NIGHT);
     String timeOfDay = this.dataSender.createInfo("TIME", DAYTIME.toString(this.gameData.getDayTime()));
     this.narrator.broadcast(timeOfDay, this.gameServer.getReadyPlayers());
-    this.sendNewsToClient();
     this.narrator.changeState(STATES.PSYCHOLOGIST);
     return false;
   }
 
-  private void sendNewsToClient() {
-    if (!this.gameData.isThereAnyNews())
-      return;
-    ArrayList<String> news = this.gameData.getNews();
-    this.narrator.broadcast(this.dataSender.createNews(news), this.gameServer.getReadyPlayers());
-    this.clearNews();
-  }
-
-  private void clearNews() {
-    this.gameData.clearNews();
-  }
 }
