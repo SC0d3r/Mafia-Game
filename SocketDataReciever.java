599ef31738148c6003a82dcb05edb87c909dceaa
SC0d3r <sasan.kelishani@gmail.com>
2021-05-24 21:22:33 +0430

    add extractGameState and remove unnecessary commands

--- SocketDataReciever.java
+++ SocketDataReciever.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -11,14 +10,12 @@ public class SocketDataReciever {
   private ArrayList<String> news;
 
   private ArrayList<String> chatHistory;
-  private volatile HashMap<String, String> votes;
   private boolean isVotingInProgress;
 
   public SocketDataReciever() {
     this.headerInformation = new HashMap<>();
     this.chatMessages = new ArrayList<>();
     this.chatHistory = new ArrayList<>();
-    this.votes = new HashMap<>();
     this.isVotingInProgress = false;
     this.news = new ArrayList<>();
   }
@@ -27,8 +24,23 @@ public class SocketDataReciever {
     this.news = new ArrayList<>();
   }
 
-  public HashMap<String, String> getVotes() {
-    return this.votes;
+  public boolean isGameStateData(String response) {
+    return response.contains(SocketDataSender.SEND_GAME_STATE);
+  }
+
+  public GameState extractGameState(String response) {
+    String data = response.split(SocketDataSender.SEPERATOR)[1];
+    return GameState.deserialize(data);
+  }
+
+  public boolean isPsychologistRequest(String respose) {
+    return respose.contains(SocketDataSender.PSYCHOLOGIST_REQUEST);
+  }
+
+  public String extractPshychologistRequest(String response) {
+    String[] datas = response.split(SocketDataSender.SEPERATOR);
+    String toBeSilenced = datas[1];
+    return toBeSilenced;
   }
 
   public void setIsVotingInProgress(boolean status) {
@@ -52,11 +64,12 @@ public class SocketDataReciever {
         || this.isRemoveHeaderInfo(response) || response.equals(SocketDataSender.DISABLE_CHAT)
         || response.equals(SocketDataSender.ENABLE_CHAT) || this.isAddAndClearChatCommand(response)
         || response.equals(SocketDataSender.ENABLE_VOTING) || response.equals(SocketDataSender.DISABLE_VOTING)
-        || this.isVotingForUserCommand(response) || this.isCreateVotingTableCommand(response)
-        || this.isVoteMapClientSideCommand(response) || response.equals(SocketDataSender.BEGIN_MAYOR_DECISIONING)
+        || this.isVotingForUserCommand(response) || response.equals(SocketDataSender.BEGIN_MAYOR_DECISIONING)
         || this.isMayorVote(response) || this.isSendPlayerStateCommand(response)
         || this.isStartMayorVotingState(response) || this.isEndMayorVotingState(response) || this.isNews(response)
-        || response.equals(SocketDataSender.CLEAR_NEWS);
+        || response.equals(SocketDataSender.CLEAR_NEWS) || response.equals(SocketDataSender.START_PSYCHOLOGIST_TURN)
+        || response.equals(SocketDataSender.END_PSYCHOLOGIST_TURN) || this.isPsychologistRequest(response)
+        || this.isGameStateData(response);
   }
 
   public boolean isNews(String response) {
@@ -104,14 +117,6 @@ public class SocketDataReciever {
     return Player.deserialize(stateString);
   }
 
-  public boolean shouldEnableVoting() {
-    return this.votes.size() > 0;
-  }
-
-  public void disableVoting() {
-    this.votes = new HashMap<>();
-  }
-
   private boolean isVotingForUserCommand(String response) {
     return response.contains(SocketDataSender.VOTE_FOR_USER);
   }
@@ -147,106 +152,20 @@ public class SocketDataReciever {
     }
   }
 
-  public void addVotingTable(String response) {
-    if (!this.isCreateVotingTableCommand(response))
-      return;
-    this.votes = new HashMap<>();
-    List<String> choices = this.extractVotingChoices(response);
-    for (String choice : choices) {
-      this.votes.put(choice, "< ! >");
-    }
-  }
-
-  private List<String> extractVotingChoices(String response) {
-    String choices = response.split(SocketDataSender.SEPERATOR)[1];
-    return Arrays.asList(choices.split(","));
-  }
-
-  private boolean isCreateVotingTableCommand(String response) {
-    return response.contains(SocketDataSender.VOTING_TABLE);
-  }
-
-  // public void addVote(String response) {
-  // if (!this.isVotingForUserCommand(response))
-  // return;
-  // // Set<String> choices = this.votes.keySet();
-  // String voteFor = this.extractVoteFor(response);
-  // String voter = this.extractVoter(response);
-  // if (!this.isValidVote(voteFor))
-  // return;
-  // this.votes.put(voter, voteFor);
-  // }
-
-  public void updateVotingTable(String response) {
-    if (!this.isVoteMapClientSideCommand(response))
-      return;
-    SocketDataSender sender = new SocketDataSender();
-    String hashString = response.split(SocketDataSender.SEPERATOR)[1];
-    this.votes = sender.turnVoteMapStringIntoHashMap(hashString);
-  }
-
-  public Set<String> getVotingChoices() {
-    return this.votes.keySet();
-  }
-
-  private String extractVoteFor(String response) {
-    String voteFor = response.split(SocketDataSender.SEPERATOR)[2];
-    return voteFor;
-  }
-
   public boolean isVotingMapForServer(String response) {
     return response.contains(SocketDataSender.VOTING_MAP_SERVER_SIDE);
   }
 
-  public void addVotingMapServerSide(String response) {
-    String voter = this.extractServerSideVoter(response);
-    String votee = this.extractServerSideVotee(response);
-    this.votes = this.createVoteHashMapServerSide(response);
-    this.votes.put(voter, votee);
-  }
-
-  private String extractServerSideVoter(String response) {
+  public String extractServerSideVoter(String response) {
     return response.split(SocketDataSender.SEPERATOR)[1];
   }
 
-  private String extractServerSideVotee(String response) {
+  public String extractServerSideVotee(String response) {
     return response.split(SocketDataSender.SEPERATOR)[2];
   }
 
-  private boolean isVoteMapClientSideCommand(String response) {
-    return response.contains(SocketDataSender.VOTING_MAP_CLIENT_SIDE);
-  }
-
-  private HashMap<String, String> createVoteHashMapServerSide(String response) {
-    HashMap<String, String> result = new HashMap<>();
-    String hashString = response.split(SocketDataSender.SEPERATOR)[3];
-    String[] keyValuePairs = hashString.split(",");
-    for (String keyValue : keyValuePairs) {
-      String key = keyValue.split(":")[0];
-      String value = keyValue.split(":")[1];
-
-      result.put(key, value);
-    }
-    return result;
-  }
-
-  public boolean isValidVote(String voteFor) {
-    Set<String> choices = this.getVotingChoices();
-    return choices.contains(voteFor);
-  }
-
-  public boolean shouldDisableChat(String response) {
-    return response.equals(SocketDataSender.DISABLE_CHAT);
-  }
-
-  public boolean shouldEnableChat(String response) {
-    return response.equals(SocketDataSender.ENABLE_CHAT);
-  }
-
   public void addChatMessage(String message) {
-    // System.out.println(message);
     if (this.isChatMessage(message)) {
-      // System.out.println(message);
       String chatMessage = this.extractChatMessage(message);
       this.chatMessages.add(chatMessage);
     }
@@ -322,16 +241,16 @@ public class SocketDataReciever {
     return String.format(result);
   }
 
-  public String getVotingTable() {
+  public String getVotingTable(HashMap<String, String> votes) {
     String result = "";
     result += String.format("+---------------------+---------------------+%n");
     result += String.format("|       Players       |        Vote         |%n");
     result += String.format("+---------------------+---------------------+%n");
     String leftAlignFormat = "| %s | %s |%n";
     // String leftAlignFormat = "| %-21s | %-21s |%n";
-    Set<String> usernames = this.votes.keySet();
+    Set<String> usernames = votes.keySet();
     for (String username : usernames) {
-      String vote = this.votes.get(username);
+      String vote = votes.get(username);
       vote = vote == null ? "< ! >" : vote;
       result += String.format(leftAlignFormat, centerString(19, username), centerString(19, vote));
     }
