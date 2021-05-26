11412c5bd640cba5dd480557a518833fc0ad3901
SC0d3r <sasan.kelishani@gmail.com>
2021-05-26 19:09:16 +0430

    add 2 second delay before the game finishes

--- CitizenWinnerState.java
+++ CitizenWinnerState.java
@@ -1,25 +1,17 @@
 public class CitizenWinnerState extends ServerState {
-
-  private SocketDataSender dataSender;
   private GameData gameData;
 
-  public CitizenWinnerState(Narrator narrator, GameServer server, SocketDataSender dataSender, GameData gameData) {
+  public CitizenWinnerState(Narrator narrator, GameServer server, GameData gameData) {
     super(narrator, server);
-    this.dataSender = dataSender;
     this.gameData = gameData;
   }
 
   @Override
   public boolean run() {
-    // this.announceCitizensWinner();
     this.gameData.setDidMafiaWin(false);
 
-    // UTIL.setTimerFor(10, this.gameServer.getReadyPlayers());
+    UTIL.setTimerFor(2, this.gameServer.getReadyPlayers());
+
     return true;// means game is finished
   }
-
-  // private void announceCitizensWinner() {
-  // String message = this.dataSender.createChatCommand(" CITIZEN WON THE GAME ");
-  // this.narrator.broadcast(message, this.gameServer.getReadyPlayers());
-  // }
 }
--- MafiaWinnerState.java
+++ MafiaWinnerState.java
@@ -1,25 +1,18 @@
 public class MafiaWinnerState extends ServerState {
-  private SocketDataSender dataSender;
   private GameData gameData;
 
-  public MafiaWinnerState(Narrator narrator, GameServer server, SocketDataSender dataSender, GameData gameData) {
+  public MafiaWinnerState(Narrator narrator, GameServer server, GameData gameData) {
     super(narrator, server);
-    this.dataSender = dataSender;
     this.gameData = gameData;
   }
 
   @Override
   public boolean run() {
-    // this.announceMafiaWinner();
     this.gameData.setDidMafiaWin(true);
 
-    // UTIL.setTimerFor(10, this.gameServer.getReadyPlayers());
+    UTIL.setTimerFor(2, this.gameServer.getReadyPlayers());
+
     return true;// means game is finished
   }
-
-  // private void announceMafiaWinner() {
-  // String message = this.dataSender.createChatCommand(" MAFIA WON THE GAME ");
-  // this.narrator.broadcast(message, this.gameServer.getReadyPlayers());
-  // }
 
 }
