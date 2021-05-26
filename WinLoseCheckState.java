a8e8b6c1b8bcec3c6f4aa35b33c25dfa57d78a2e
SC0d3r <sasan.kelishani@gmail.com>
2021-05-26 17:47:29 +0430

    fix winLoseState checks for mafia and citizens

--- WinLoseCheckState.java
+++ WinLoseCheckState.java
@@ -10,9 +10,11 @@ public class WinLoseCheckState extends ServerState {
   public boolean run() {
     if (this.didMafiaWin()) {
       this.narrator.changeState(STATES.MAFIA_WINNER);
+      return false;
     }
     if (this.didCitizenWin()) {
       this.narrator.changeState(STATES.CITIZEN_WINNER);
+      return false;
     }
 
     this.narrator.changeState(STATES.ENABLE_CHAT);
@@ -20,29 +22,12 @@ public class WinLoseCheckState extends ServerState {
   }
 
   public boolean didCitizenWin() {
-    return this.getMafias().size() == 0;
+    return this.gameServer.getGameState().getAliveMafiaUsernames().size() == 0;
   }
 
   private boolean didMafiaWin() {
-    return this.getMafias().size() >= this.getCitizens().size();
+    return this.gameServer.getGameState().getAliveMafiaUsernames().size() >= this.gameServer.getGameState()
+        .getAliveCitizenUsernames().size();
   }
 
-  private ArrayList<Player> getMafias() {
-    ArrayList<Player> result = new ArrayList<>();
-    for (Player p : this.gameServer.getReadyPlayers()) {
-      if (p.getRole() == ROLE.GOD_FATHER || p.getRole() == ROLE.MAFIA_MEMBER || p.getRole() == ROLE.DR_LACTER)
-        result.add(p);
-    }
-    return result;
-  }
-
-  public ArrayList<Player> getCitizens() {
-    ArrayList<Player> result = new ArrayList<>();
-    for (Player p : this.gameServer.getReadyPlayers()) {
-      if (p.getRole() == ROLE.GOD_FATHER || p.getRole() == ROLE.DR_LACTER || p.getRole() == ROLE.MAFIA_MEMBER)
-        continue;
-      result.add(p);
-    }
-    return result;
-  }
 }
